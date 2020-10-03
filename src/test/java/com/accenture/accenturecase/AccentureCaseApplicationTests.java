a86cd3f81bdbdd4b67e339c36e0727a7aed4631f
Jeremias Ponciano Da Silva <jeremiason2911@gmail.com>
2020-10-03 16:53:46 -0300

    add arquivos sprint

---
+++ src/test/java/com/accenture/accenturecase/AccentureCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accenture.accenturecase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccentureCaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
